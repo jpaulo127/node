55bda17b1dac10f9b9829156f95bce820be07289
joao paulo Oliveira <joaopaulooliveira102@gmail.com>
2023-06-15 00:52:08 -0300

    node.js

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-const http = require('http');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const server = express()
 
-const host = '127.0.0.1';
-const port = 3000;
+server.listen(3001)
+server.use(cors())
+server.use(bodyParser.urlencoded({ extended: true }))
 
-const server = http.createServer( (req, res) =>{
-    res.statusCode = 200;
-    res.setHeader('content-type', 'text/plain');
-    res.end('hello World');
+server.use(express.json())
+const contacts = [
+    {"name": "Marcelo"},
+    {"name": "Maria"},
+    {"name": "Joao"},
+    {"name": "fulando"}
+]
+
+
+server.get('/', () => {
+           console.log('Excutando a rota test com o GET + nodemon atualizado')
+
 })
 
-server.listen( port, host, ()=>{
-    console.log(`servidor rodando em http://${host}:${port}/`)
+//Lista
+server.get('/contact', (req, res) => {
+    return res.json(contacts)
+
+})
+
+//Adiciona
+server.post('/contact', (req, res) =>{
+    const { name } = req.body
+    contacts.push({"name": name})
+    return res.json(contacts)
+})
+
+//Atualizar
+server.put('/contact/:index', (req, res) =>{
+    const { index } = req.params
+    const { name } = req.body
+
+    contacts[index] = { "name": name}
+
+    return res.json(contacts)
+
+})
+
+//Delete
+server.delete('/contact/:index', (req, res) =>{
+    const { index}  = req.params
+    contacts.splice(index,1)
+
+    return res.json(contacts)
 })
--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
+//const { response } = require("express")
+
 const elem = document.getElementById('title')
 elem.innerHTML = 'rodando o node com webopack'
+
+const list = document.getElementById('list')
+const api = 'http://localhost:3001/contact'
+
+//fetch api
+fetch( api, {method: 'get'} )
+   .then( (response)=> response.json())
+   .then( function(data){
+    data.map( contact => {
+        let li = document.createElement('li')
+        li.innerHTML = contact.name
+        list.appendChild(li)
+    })
+   })
+
